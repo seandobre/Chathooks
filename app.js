629c40eb6cf3091fb9fc221a953acf2494964fb5
seandobre <113632554+seandobre@users.noreply.github.com>
2022-12-24 15:15:49 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const port =  3000
 const fetch = require('node-fetch');
 const app = express();
+const hostname = '0.0.0.0';
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded());
